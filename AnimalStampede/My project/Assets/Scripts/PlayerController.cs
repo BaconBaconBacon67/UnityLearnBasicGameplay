65c3f0eed2a972150a44abd9f9cf66d914748262
James Munjone <munjonej1@roverkids.org>
2026-04-23 13:50:37 -0400

    2.2 Complete

--- AnimalStampede/My project/Assets/Scripts/PlayerController.cs
+++ AnimalStampede/My project/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 10f;
     public float xRange = 10f;
 
+    public GameObject projectilePrefab;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,6 +18,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        // check for spacebar click 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // launch projectile from player 
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation); 
+           }
+        // contin left/right movement
         if(transform.position.x < -xRange)
         { transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
                 
